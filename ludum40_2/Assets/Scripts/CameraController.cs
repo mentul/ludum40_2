cdac28702ecf372960f055fea8ef1515ab501d14
Krzychu-Lab\Krzychu <dainarudd@gmail.com>
2017-12-02 12:16:27 +0100

    kamera

--- ludum40_2/Assets/Scripts/CameraController.cs
+++ ludum40_2/Assets/Scripts/CameraController.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+	private Transform carTransform;
 
 	public void DoInit ()
 	{
-
+		carTransform = GameController.Current.car.transform;
+		transform.position = carTransform.position + Vector3.back * 10f;
+		transform.SetParent (carTransform);
 	}
 
 	public void DoUpdate ()
