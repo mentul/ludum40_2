52b5a4d07bae28a3f4e7592d7fdc21ed01016e3c
Grzegorz Dziembowski <grzegorz.dziembowski@matrix42ext.onmicrosoft.com>
2017-12-02 12:07:53 +0100

    add for controller

--- ludum40_2/Assets/Scripts/CarController.cs
+++ ludum40_2/Assets/Scripts/CarController.cs
@@ -4,13 +4,60 @@ using UnityEngine;
 
 public class CarController : MonoBehaviour {
 
+	private int damage = 0;
+	private int coins = 0;
+	private int fameLevel = 0;
+	private int lap = 0;
+
 	// Use this for initialization
-	void Start () {
+	public void DoInit () {
 		
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	public void DoUpdate () {
 		
 	}
+
+
+	void OnTriggerEnter2D( Collider2D other )
+	{
+		if (other.CompareTag("box")) 
+		{
+			this.damage += 10;
+		}
+		else if (other.CompareTag("barrel"))
+		{
+			this.damage += 15;
+		}
+		else if (other.CompareTag("barrier"))
+		{
+			this.damage += 20;
+		}
+		else if (other.CompareTag("coins")) 
+		{
+			this.coins++;
+			this.UpdateFameLevel ();
+		};
+		this.UpdateGameStatus ();
+	}
+
+
+
+	private void UpdateGameStatus()
+	{
+
+	}
+
+	public void UpdateFameLevel()
+	{
+		fameLevel = coins / 10 * lap;
+	}
+
+	public void UpdateLapLevel(int lapCount)
+	{
+		this.lap = lapCount;
+		this.UpdateFameLevel ();
+	}
+
 }
